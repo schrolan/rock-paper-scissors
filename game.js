ad1ff74899b01f2d3dae249884312107511b21cb
schrolan <schrolan000@gmail.com>
2022-09-12 19:40:13 -0500

    added psuedocode

--- game.js
+++ game.js
@@ -1,1 +1,44 @@
-console.log("yo!")
+//psuedocoding: it's basically just putting what you need others to do or even a way to remind yourself what to do.
+
+//define age variables
+//define name variable
+
+//if age is greater than or equal to 17 blah blah blah
+
+
+
+
+
+
+//define array for choices {r,p,s}
+//define var for computer choices
+//define var for player choice
+//define var for wins
+//define var for losses
+//define var for ties
+
+
+//prompt humanChoice
+
+//randomly choose comptuter choice
+
+//compare choices
+    //if CC is p and hC is p then tie
+    //if CC is p and hC is r then loss
+    //if CC is p and hC is s then win
+    //if CC is s and hC is p then loss
+    //if CC is s and hC is r then win
+    //if CC is s and hC is s then tie
+    //if CC is r and hC is p then win
+    //if CC is r and hC is r then tie
+    //if CC is r and hC is s then loss
+
+
+//display (alert) comparison results (won, tied, lost round)
+
+//show stats (number of wins, losses, ties)
+
+//play again?
+    //restart game
+//else
+    //end game
